68ef8f0a86d3da8541c34af6299c123ee8d62005
mustafaevf <knetya274@gmail.com>
2023-07-24 00:22:08 +0300

    top, bottom

--- index.php
+++ index.php
@@ -4,10 +4,44 @@ ini_set('display_errors', 1);
 
 $url = substr($_SERVER['REQUEST_URI'], 1);
 
-var_dump($url);
-
 if($url == '') {
     include "views/index.php";
+} elseif ($url == 'login') {
+    include "views/".$url;
+} elseif ($url == 'register') {
+    include "views/".$url;
+}
+
+
+function top($title) {
+    echo '<html>
+            <head>
+                <title>'.$title.'</title>
+                <link rel="stylesheet" href="/public/css/style.css">
+            </head>
+            <body>
+                <header>
+                    <div class="header-content">
+                        <div class="header-logo">
+                            <img src="https://codechange.cc/assets/image/logo.svg" alt="logo">
+                        </div>
+                        <div class="header-nav">
+                            <a href="">Торговая площадка</a>
+                            <a href="">Игры</a>
+                            <a href="">О нас</a>
+                            <div class="line"></div>
+                            <a href="/login" class="button">Авторизация</a>
+                        </div>
+                    </div>
+                </header>';
+
+}
+
+function bottom() {
+    echo '
+            </body>
+
+        </html>';
 }
 
 ?>
--- views/index.php
+++ views/index.php
@@ -1,28 +1,7 @@
-<html>
-<head>
-    <title>Main page</title>
-    <link rel="stylesheet" href="/public/css/style.css">
-</head>
-<body>
-    <header>
-        <div class="header-content">
-            <div class="header-logo">
-                <img src="https://codechange.cc/assets/image/logo.svg" alt="logo">
-            </div>
-            <div class="header-nav">
-                <a href="">Торговая площадка</a>
-                <a href="">Игры</a>
-                <a href="">О нас</a>
-                <div class="line"></div>
-                <a href="" class="button">Авторизация</a>
-            </div>
-        </div>
-    </header>
+<?php top('Главная страница') ?>
     
-    <div class="content">
-        <p class="big-title">Обзор</p>
+<div class="content">
+    <p class="big-title">Обзор</p>
+</div>
 
-    </div>
-</body>
-
-</html>
+<?php bottom() ?>
