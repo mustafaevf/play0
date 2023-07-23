8d815770d013033a6fa9f508a28825c53cd70b42
mustafaevf <knetya274@gmail.com>
2023-07-24 00:48:53 +0300

    jquery

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+function login() {
+    const login = $('#input-login').val();
+    const password = $('#input-password').val();
+}
+
+function register() {
+    const login = $('#input-login').val();
+    const password = $('#input-password').val();
+    const retry_password = $('#input-retry-password').val(); 
+}
